17eb1af15efd08dcf52a352d2deb72a7352a5091
Shankar <shankar.n@ethnus.com>
2023-04-25 01:45:14 +0530

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ function About(){
         <div>
             <h1>About Page</h1>
             <a href="/router-example/#/">Home</a>
-            <a href="router-example/#//about">About</a>
+            <a href="/router-example/#/about">About</a>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ function Home() {
         <div>
             <h1>Home Page</h1>
             <a href="/router-example/#/">Home</a>
-            <a href="router-example/#//about">About</a>
+            <a href="/router-example/#/about">About</a>
         </div>
     )
 }
