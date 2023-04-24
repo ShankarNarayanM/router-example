47a4463de544ed9acf16fa183e72e0a33b219541
Shankar <shankar.n@ethnus.com>
2023-04-25 01:20:12 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+function About(){
+    return (
+        <div>
+            <h1>About Page</h1>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+        </div>
+    )
+}
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+        </div>
+    )
+}
+export default Home;
